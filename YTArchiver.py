ed652d90a11401aa574c1fdf4636c09fdd064559
skurbee <scottbarks@gmail.com>
2026-03-14 19:15:16 -0500

    date

--- YTArchiver.py
+++ YTArchiver.py
@@ -2288,7 +2288,7 @@ header_strip.pack(fill="x", side="top")
 header_strip.pack_propagate(False)
 tk.Label(header_strip, text="YT ARCHIVER", bg=C_BG, fg=C_TEXT,
          font=("Segoe UI Semibold", 13), anchor="w").pack(side="left", padx=16, pady=10)
-tk.Label(header_strip, text="v16.9 - 03.14.26 2:55pm", bg=C_BG, fg=C_DIM,
+tk.Label(header_strip, text="v17.0 - 03.14.26 7:14pm", bg=C_BG, fg=C_DIM,
          font=("Segoe UI", 8), anchor="w").pack(side="left", pady=14)
 tk.Frame(root, bg=C_BORDER_LT, height=1).pack(fill="x", side="top")
 

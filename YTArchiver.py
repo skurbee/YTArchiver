92191d8c3285e7ae5a273673d03dd3e4771328b2
skurbee <scottbarks@gmail.com>
2026-03-16 19:04:32 -0500

    Update version label in YTArchiver.py

--- YTArchiver.py
+++ YTArchiver.py
@@ -2674,7 +2674,7 @@ header_strip.pack(fill="x", side="top")
 header_strip.pack_propagate(False)
 tk.Label(header_strip, text="YT ARCHIVER", bg=C_BG, fg=C_TEXT,
          font=("Segoe UI Semibold", 13), anchor="w").pack(side="left", padx=16, pady=10)
-tk.Label(header_strip, text="v19.1 - 03.16.26 11:35pm", bg=C_BG, fg=C_DIM,
+tk.Label(header_strip, text="v19.1 - 03.16.26 7:04pm", bg=C_BG, fg=C_DIM,
          font=("Segoe UI", 8), anchor="w").pack(side="left", pady=14)
 tk.Frame(root, bg=C_BORDER_LT, height=1).pack(fill="x", side="top")
 
